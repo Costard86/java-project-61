b889fb3ac7ebec3f65650a2a5136db17b72b7ea4
Costard86 <Costard86@gmail.com>
2023-05-07 10:48:34 +0700

    add greet

---
+++ app/src/main/java/hexlet/code/Cli.java
@@ -0,0 +1,10 @@
+package hexlet.code;
+import java.util.Scanner;
+public class Cli {
+    public static void greet() {
+        System.out.println("May I have your name?");
+        Scanner sc = new Scanner(System.in);
+        String name = sc.nextLine();
+        System.out.println("Hello, " + name + "!");
+    }
+}
