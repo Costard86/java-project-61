c84356a89176b29a9959eb308210bbb84d4fa1b8
Costard86 <Costard86@gmail.com>
2023-05-12 13:23:41 +0700

    fix App Linter

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -2,7 +2,8 @@ package hexlet.code;
 import java.util.Scanner;
 public class App {
     public static void main(String[] args) {
-        System.out.println("Please enter the game number and press Enter.\n1 - Greet\n2 - Even\n0 - Exit\nYour choice:");
+        System.out.println("Please enter the game number and press Enter.\n1 - Greet\n2 - Even\n0 - Exit\n"
+                + "Your choice:");
         Scanner scan = new Scanner(System.in);
         int numberGame = scan.nextInt();
         switch (numberGame) {
