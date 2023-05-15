9af0a0a3de4aec5f148a4c16cc2d2d599b6b8db2
Costard86 <Costard86@gmail.com>
2023-05-15 19:46:20 +0700

    add GCD game

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -2,14 +2,15 @@ package hexlet.code;
 import java.util.Scanner;
 public class App {
     public static void main(String[] args) {
-        System.out.println("Please enter the game number and press Enter.\n1 - Greet\n2 - Even\n3 - Calc\n0 - Exit\n"
-                + "Your choice:");
+        System.out.println("Please enter the game number and press Enter.\n1 - Greet\n2 - Even\n3 - Calc\n4 - GCD\n"
+                + "0 - Exit\nYour choice:");
         Scanner scan = new Scanner(System.in);
         int numberGame = scan.nextInt();
         switch (numberGame) {
             case 1 -> Cli.greet();
             case 2 -> Even.even();
             case 3 -> Calc.calc();
+            case 4 -> GCD.gcd();
             case 0 -> System.out.println("Goodbye!");
             default -> System.out.println("Invalid choice, please try again!");
         }
---
+++ app/src/main/java/hexlet/code/GCD.java
@@ -0,0 +1,42 @@
+package hexlet.code;
+
+import java.util.Scanner;
+
+public class GCD {
+    public static void gcd() {
+        System.out.println("Welcome to the Brain Games!");
+        System.out.println("May I have your name?");
+        Scanner sc = new Scanner(System.in);
+        String name = sc.nextLine();
+        System.out.println("Hello, " + name + "!");
+        System.out.println("Find the greatest common divisor of given numbers.");
+        var attempts = 0;
+        while (attempts < 3) {
+            int randomNumb1 = 1 + (int) (Math.random() * 50);
+            int randomNumb2 = 1 + (int) (Math.random() * 50);
+            System.out.println("Question: " + randomNumb1 + " " + randomNumb2 + "\nYour answer: ");
+            while (randomNumb1 != 0 && randomNumb2 != 0) {
+                if (randomNumb1 > randomNumb2) {
+                    randomNumb1 = randomNumb1 % randomNumb2;
+                } else {
+                    randomNumb2 = randomNumb2 % randomNumb1;
+                }
+            }
+            int result = randomNumb1 + randomNumb2;
+
+            Scanner scan = new Scanner(System.in);
+            int number = scan.nextInt();
+            if (number == result) {
+                System.out.println("Correct!");
+                attempts = attempts + 1;
+            } else {
+                System.out.println('\'' + Integer.toString(number) + '\'' + " is wrong answer ;(. Correct answer was "
+                        + '\'' + result + '\'' + "\nLet's try again, " + name + '!');
+                break;
+            }
+        }
+        if (attempts > 2) {
+            System.out.println("Congratulations, " + name + '!');
+        }
+    }
+}
