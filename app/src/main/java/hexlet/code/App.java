1c3ed0b03169c4e1f1dd1fb9f738f9994c899bff
Costard86 <Costard86@gmail.com>
2023-05-16 13:23:03 +0700

    add Progression game

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,9 +1,10 @@
 package hexlet.code;
 import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) {
         System.out.println("Please enter the game number and press Enter.\n1 - Greet\n2 - Even\n3 - Calc\n4 - GCD\n"
-                + "0 - Exit\nYour choice:");
+                + "5 - Progression\n0 - Exit\nYour choice:");
         Scanner scan = new Scanner(System.in);
         int numberGame = scan.nextInt();
         switch (numberGame) {
@@ -11,6 +12,7 @@ public class App {
             case 2 -> Even.even();
             case 3 -> Calc.calc();
             case 4 -> GCD.gcd();
+            case 5 -> Progression.progression();
             case 0 -> System.out.println("Goodbye!");
             default -> System.out.println("Invalid choice, please try again!");
         }
---
+++ app/src/main/java/hexlet/code/Progression.java
@@ -0,0 +1,48 @@
+package hexlet.code;
+import java.util.Scanner;
+
+public class Progression {
+    public static void progression() {
+        System.out.println("Welcome to the Brain Games!");
+        System.out.println("May I have your name?");
+        Scanner sc = new Scanner(System.in);
+        String name = sc.nextLine();
+        System.out.println("Hello, " + name + "!");
+        System.out.println("What number is missing in the progression?");
+        var attempts = 0;
+        while (attempts < 3) {
+            int startElement = 1 + (int) (Math.random() * 10);
+            int step = 1 + (int) (Math.random() * 5);
+            var progression = new String[10];
+
+            for (var i = 0; i < 10; i++) {
+                progression[i] = Integer.toString(startElement);
+                startElement = startElement + step;
+            }
+
+            int randomMissElem = (int) (Math.random() * 10);
+            var temp = progression[randomMissElem];
+            progression[randomMissElem] = "..";
+            String masToString = "";
+            for (var i = 0; i < 10; i++) {
+                masToString = masToString + progression[i] + " ";
+            }
+
+            System.out.println("Question: " + masToString + "\nYour answer: ");
+
+            Scanner answer = new Scanner(System.in);
+            String userAnswer = answer.nextLine();
+            if (userAnswer.equals(temp)) {
+                System.out.println("Correct!");
+                attempts = attempts + 1;
+            } else {
+                System.out.println('\'' + userAnswer + '\'' + " is wrong answer ;(. Correct answer was "
+                        + temp + "\nLet's try again, " + name + '!');
+                break;
+            }
+        }
+        if (attempts > 2) {
+            System.out.println("Congratulations, " + name + '!');
+        }
+    }
+}
