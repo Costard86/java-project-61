347680426574cd662be4f6f7c7e57df858add857
Costard86 <Costard86@gmail.com>
2023-05-17 11:32:25 +0700

    add Prime game

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -3,8 +3,8 @@ import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Please enter the game number and press Enter.\n1 - Greet\n2 - Even\n3 - Calc\n4 - GCD\n"
-                + "5 - Progression\n0 - Exit\nYour choice:");
+        System.out.println("Please enter the game number and press Enter.\n1 - Greet\n2 - Even\n3 - Calc\n4 - GCD");
+        System.out.println("5 - Progression\n6 - Prime\n0 - Exit\nYour choice:");
         Scanner scan = new Scanner(System.in);
         int numberGame = scan.nextInt();
         switch (numberGame) {
@@ -13,6 +13,7 @@ public class App {
             case 3 -> Calc.calc();
             case 4 -> GCD.gcd();
             case 5 -> Progression.progression();
+            case 6 -> Prime.prime();
             case 0 -> System.out.println("Goodbye!");
             default -> System.out.println("Invalid choice, please try again!");
         }
---
+++ app/src/main/java/hexlet/code/Prime.java
@@ -0,0 +1,44 @@
+package hexlet.code;
+import java.util.Scanner;
+
+public class Prime {
+    public static void prime() {
+        System.out.println("Welcome to the Brain Games!");
+        System.out.println("May I have your name?");
+        Scanner sc = new Scanner(System.in);
+        String name = sc.nextLine();
+        System.out.println("Hello, " + name + "!");
+        System.out.println("What number is missing in the progression?");
+        var attempts = 0;
+        while (attempts < 3) {
+            int number = 1 + (int) (Math.random() * 100);
+            String correctAnswer = "yes";
+            if (number < 2) {
+                correctAnswer = "no";
+            }
+            var divider = 2;
+            while (divider <= number / 2) {
+                if (number % divider == 0) {
+                    correctAnswer = "no";
+                }
+                divider++;
+            }
+            var strNumber = Integer.toString(number);
+            System.out.println("Question: " + strNumber + "\nYour answer: ");
+
+            Scanner answer = new Scanner(System.in);
+            String userAnswer = answer.nextLine();
+            if (userAnswer.equals(correctAnswer)) {
+                System.out.println("Correct!");
+                attempts = attempts + 1;
+            } else {
+                System.out.println('\'' + userAnswer + '\'' + " is wrong answer ;(. Correct answer was "
+                        + '\'' + correctAnswer + '\'' + "\nLet's try again, " + name + '!');
+                break;
+            }
+        }
+        if (attempts > 2) {
+            System.out.println("Congratulations, " + name + '!');
+        }
+    }
+}
